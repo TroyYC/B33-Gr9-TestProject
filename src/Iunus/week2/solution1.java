cbb059e1331a5f43fd51618d82afa91e0fb37f3d
Iunus <yunus_magdiev01@mail.ru>
2024-03-27 16:46:22 -0400

    check it

--- src/Iunus/week2/solution1.java
+++ src/Iunus/week2/solution1.java
@@ -3,6 +3,9 @@ package Iunus.week2;
 import org.testng.annotations.Test;
 
 public class solution1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 
 
 
---
+++ src/Iunus/week2/solution2.java
@@ -0,0 +1,7 @@
+package Iunus.week2;
+
+public class solution2 {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
